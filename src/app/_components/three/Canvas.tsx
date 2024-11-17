475cc19dc23e5156af4a27b8b37377997a677caa
John Bogart <johnjackbogart@gmail.com>
2024-11-17 14:30:13 -0500

    bug clean up, logo should work and shouldn't freeze on resize

--- src/app/_components/three/Canvas.tsx
+++ src/app/_components/three/Canvas.tsx
@@ -1,22 +1,37 @@
 "use client";
-import { Suspense } from "react";
+import { useState, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Effects } from "./Effects";
 import { Loader } from "./Loader";
 import { Scene } from "./Scene";
 
 function ThreeCanvas() {
+  //well we figured out how to break the vertexShader
+  const [render, setRender] = useState(true);
+  const handleMouseEnter = () => {
+    setRender(true);
+  };
+  const handleMouseLeave = () => {
+    setRender(false);
+  };
+
   return (
-    <Canvas
-      gl={{ alpha: true }}
-      eventPrefix="client"
-      camera={{ position: [0, 0, 0], fov: 100 }}
+    <div
+      className={"flex-grow"}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
-      <Suspense fallback={<Loader />}>
-        <Effects />
-        <Scene />
-      </Suspense>
-    </Canvas>
+      <Canvas
+        gl={{ alpha: true }}
+        eventPrefix="client"
+        camera={{ position: [0, 0, 0], fov: 100 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <Effects />
+          <Scene />
+        </Suspense>
+      </Canvas>
+    </div>
   );
 }
 
