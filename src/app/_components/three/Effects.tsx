e23c1e3a6bec01dcc3c63bf3385e5783cd917f63
John Bogart <johnjackbogart@gmail.com>
2025-08-30 14:34:43 -0400

    testing mobile fixes

--- src/app/_components/three/Effects.tsx
+++ src/app/_components/three/Effects.tsx
@@ -4,6 +4,7 @@ import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
 import { easing } from "maath";
+import isMobile from "ismobilejs";
 import { useThemeToFill } from "&/theme";
 
 type EffectsProps = {
@@ -23,15 +24,20 @@ function Effects({
   const theming = useThemeToFill();
   const dark = theming?.theme === "dark" ? true : false;
 
+  const mobileModifier = isMobile(window.navigator) ? 0.01 : 0;
   useFrame((state, delta) => {
     //can I just import this as a prop ?????
     //stolen from https://discourse.threejs.org/t/how-to-create-glass-material-that-refracts-elements-in-dom/53625/3
     easing.damp3(
       state.camera.position,
       [
-        cameraModifier.x * Math.sin(0.01 * -state.pointer.x) * 50,
-        cameraModifier.y * 1 * state.pointer.y * 2,
-        cameraModifier.z * 0.5 + Math.cos(0.01 * state.pointer.x) * 5,
+        mobileModifier *
+          cameraModifier.x *
+          Math.sin(0.01 * -state.pointer.x) *
+          50,
+        mobileModifier * cameraModifier.y * 1 * state.pointer.y * 2,
+        mobileModifier * cameraModifier.z * 0.5 +
+          Math.cos(0.01 * state.pointer.x) * 5,
       ],
       damping,
       delta,
